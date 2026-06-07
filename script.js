2e6b954c4c31b47c9831c953f646812d71bca26f
zadook1698-oss <zadook1698@gmail.com>
2026-06-07 00:18:29 -0400

    Remove event listener for DOMContentLoaded

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btnModo = document.getElementById("btn-modo");
-    const body = document.body;
 
-    btnModo.addEventListener("click", () => {
-        // Si estamos en modo mover, pasamos a modo links
-        if (body.classList.contains("modo-mover")) {
-            body.classList.remove("modo-mover");
-            body.classList.add("modo-links");
-            btnModo.innerHTML = "🔗 Modo: Hacer Clic";
-        } 
-        // Si estamos en modo links, volvemos a modo mover
-        else {
-            body.classList.remove("modo-links");
-            body.classList.add("modo-mover");
-            btnModo.innerHTML = "✋ Modo: Mover";
-        }
-    });
-});
